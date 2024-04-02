933b15763748b454339340f547d27545f2ac8e16
miguelscastro <miguelscastro18@gmail.com>
2024-04-02 17:07:09 -0300

    Initial commit

---
+++ scrappExercise/readme.md
@@ -0,0 +1,5 @@
+# Exercício de scrapping
+
+## Página alvo do scrapping: https://fatecrl.edu.br/cursos/sistemas-para-internet/grades
+
+O objetivo era coletar os nomes das materias, seus objetivos e ementas separadamente. É possível navegar pelas funções apenas digitando o que busca dentro da grade dentro de escolha().
---
+++ scrappExercise/scrappGrades.js
@@ -0,0 +1,67 @@
+function objetivos() {
+    let ciclos = document.querySelectorAll(".tab-grid-content")
+    for (let i = 0; i < ciclos.length; i++) {
+        const divMaterias = ciclos[i]
+
+        console.log("Ciclo: ", i + 1);
+
+        for (let i = 0; i < divMaterias.children.length; i++) {
+            const materias = divMaterias.children[i]
+
+            for (let i = 0; i < materias.children.length; i++) {
+                const disciplina = materias.children[i]
+                const objetivo = disciplina.children[1]
+
+                console.log(objetivo.children[0].innerText, objetivo.children[1].innerText)
+            }
+        }
+    }
+}
+function ementas() {
+    let ciclos = document.querySelectorAll(".tab-grid-content")
+    for (let i = 0; i < ciclos.length; i++) {
+        const divMaterias = ciclos[i]
+
+        console.log("Ciclo: ", i + 1);
+
+        for (let i = 0; i < divMaterias.children.length; i++) {
+            const materias = divMaterias.children[i]
+
+            for (let i = 0; i < materias.children.length; i++) {
+                const disciplina = materias.children[i]
+                const ementa = disciplina.children[1]
+
+                console.log(ementa.children[2].innerText, ementa.children[3].innerText)
+            }
+        }
+    }
+}
+function materias() {
+    let ciclos = document.querySelectorAll(".tab-grid-content")
+    for (let i = 0; i < ciclos.length; i++) {
+        const divMaterias = ciclos[i]
+
+        console.log("Ciclo: ", i + 1);
+
+        for (let i = 0; i < divMaterias.children.length; i++) {
+            const materias = divMaterias.children[i]
+
+            for (let i = 0; i < materias.children.length; i++) {
+                const disciplina = materias.children[i]
+                const nome = disciplina.children[0]
+
+                console.log(nome.children[0].innerText)
+            }
+        }
+    }
+}
+function escolha(alvo){
+    switch (alvo) {
+        case objetivos: objetivos()
+        break;
+        case ementas: ementas() 
+        break;
+        case materias: materias()
+        break;
+    }
+}
